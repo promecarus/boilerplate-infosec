ebe9880297f6edf3ddb62634e3cf9daaaf422069
promecarus <haikalslipi@gmail.com>
2022-12-30 20:59:02 +0700

    Ask Browsers to Access Your Site via HTTPS Only

--- myApp.js
+++ myApp.js
@@ -12,7 +12,11 @@ app.use(helmet.noSniff());
 
 app.use(helmet.ieNoOpen());
 
-
+let ninetyDaysInSeconds = 90 * 24 * 60 * 60;
+app.use(helmet.hsts({
+	maxAge: ninetyDaysInSeconds,
+	force: true 
+}));
 
 
 
